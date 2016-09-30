baa3a02719b9ca4d24bf8312bf09e0ae78b9d6f6
Aleksandr Andrienko <aandrienko@codenvy.com>
2016-09-30 12:14:16 +0300

    Realize delete files immediatly after use method clear in the LocalWorkspaceFSStorageCleanerImpl

--- plugins/plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/LocalWorkspaceFSStorageCleanerImpl.java
+++ plugins/plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/LocalWorkspaceFSStorageCleanerImpl.java
@@ -10,18 +10,24 @@
  *******************************************************************************/
 package org.eclipse.che.plugin.docker.machine;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 
-import org.eclipse.che.api.core.util.FileCleaner;
 import org.eclipse.che.api.workspace.server.WorkspaceFSStorageCleaner;
+import org.eclipse.che.commons.lang.IoUtil;
+import org.eclipse.che.commons.lang.concurrent.ThreadLocalPropagateContext;
 import org.eclipse.che.plugin.docker.machine.node.WorkspaceFolderPathProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Local implementation of the {@link WorkspaceFSStorageCleaner}.
@@ -34,6 +40,7 @@ public class LocalWorkspaceFSStorageCleanerImpl implements WorkspaceFSStorageCle
     private static final Logger LOG = LoggerFactory.getLogger(LocalWorkspaceFSStorageCleanerImpl.class);
 
     private final WorkspaceFolderPathProvider workspaceFolderPathProvider;
+    private final ExecutorService             executor;
 
     @Inject(optional = true)
     @Named("host.projects.root")
@@ -42,18 +49,23 @@ public class LocalWorkspaceFSStorageCleanerImpl implements WorkspaceFSStorageCle
     @Inject
     public LocalWorkspaceFSStorageCleanerImpl(WorkspaceFolderPathProvider workspaceFolderPathProvider) {
         this.workspaceFolderPathProvider = workspaceFolderPathProvider;
+        executor = Executors.newSingleThreadExecutor(new ThreadFactoryBuilder().setNameFormat("LocalWorkspaceFSStorageCleanerImpl-%d")
+                                                                               .setDaemon(true)
+                                                                               .build());
     }
 
     @Override
     public void clear(String workspaceId) {
-        try {
-            String workspacePath = workspaceFolderPathProvider.getPath(workspaceId);
-            File workspaceStorage = new File(workspacePath);
-            if (!workspacePath.equals(hostProjectsFolder) && workspaceStorage.exists()) {
-                FileCleaner.addFile(workspaceStorage);
+        ThreadLocalPropagateContext.wrap(() -> {
+            try {
+                String workspacePath = workspaceFolderPathProvider.getPath(workspaceId);
+                Path workspaceStorage = Paths.get(workspacePath);
+                if (!workspacePath.equals(hostProjectsFolder) && Files.exists(workspaceStorage)) {
+                    IoUtil.deleteRecursive(workspaceStorage.toFile());
+                }
+            } catch (IOException e) {
+                LOG.error("Failed to clean up workspace folder for workspace with id: {}. Cause: {}.", workspaceId, e.getMessage());
             }
-        } catch (IOException e) {
-            LOG.error("Failed to clean up workspace folder for workspace with id: {}. Cause: {}.", workspaceId, e.getMessage());
-        }
+        });
     }
 }
